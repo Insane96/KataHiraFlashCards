27c117c4aa3bd419ebb61557ef035859c22ecb82
Insane96 <delvillano.alberto@gmail.com>
2024-12-05 14:28:21 +0100

    Abemus chars and groups

--- app/src/main/java/com/github/insane96/katahiraflashcards/JChar.java
+++ app/src/main/java/com/github/insane96/katahiraflashcards/JChar.java
@@ -1,130 +1,129 @@
 package com.github.insane96.katahiraflashcards;
 
 public enum JChar {
-    A('あ', 'ア', "a"),
-    I('い', 'イ', "i"),
-    U('う', 'ウ', "u"),
-    E('え', 'ェ', "e"),
-    O('お', 'ォ', "o"),
+    A("あ", "ア", "a"),
+    I("い", "イ", "i"),
+    U("う", "ウ", "u"),
+    E("え", "ェ", "e"),
+    O("お", "ォ", "o"),
 
-    KA('か', 'カ', "ka"),
-    KI('き', 'キ', "ki"),
-    KU('く', 'ク', "ku"),
-    KE('け', 'ケ', "ke"),
-    KO('こ', 'コ', "ko"),
+    KA("か", "カ", "ka"),
+    KI("き", "キ", "ki"),
+    KU("く", "ク", "ku"),
+    KE("け", "ケ", "ke"),
+    KO("こ", "コ", "ko"),
 
-    GA('が', 'ガ', "ga"),
-    GI('ぎ', 'ギ', "gi"),
-    GU('ぐ', 'グ', "gu"),
-    GE('げ', 'ゲ', "ge"),
-    GO('ご', 'ゴ', "go"),
+    GA("が", "ガ", "ga"),
+    GI("ぎ", "ギ", "gi"),
+    GU("ぐ", "グ", "gu"),
+    GE("げ", "ゲ", "ge"),
+    GO("ご", "ゴ", "go"),
 
-    SA('さ', 'サ', "sa"),
-    SHI('し', 'シ', "shi"),
-    SU('す', 'ス', "su"),
-    SE('せ', 'セ', "se"),
-    SO('そ', 'ソ', "so"),
+    SA("さ", "サ", "sa"),
+    SHI("し", "シ", "shi"),
+    SU("す", "ス", "su"),
+    SE("せ", "セ", "se"),
+    SO("そ", "ソ", "so"),
 
-    ZA('ざ', 'ザ', "za"),
-    JI('じ', 'ジ', "ji"),
-    ZU('ず', 'ズ', "zu"),
-    ZE('ぜ', 'ゼ', "ze"),
-    ZO('ぞ', 'ゾ', "zo"),
+    ZA("ざ", "ザ", "za"),
+    JI("じ", "ジ", "ji"),
+    ZU("ず", "ズ", "zu"),
+    ZE("ぜ", "ゼ", "ze"),
+    ZO("ぞ", "ゾ", "zo"),
 
-    TA('た', 'タ', "ta"),
-    CHI('ち', 'チ', "chi"),
-    TSU('つ', 'ツ', "tsu"),
-    TE('て', 'テ', "te"),
-    TO('と', 'ト', "to"),
+    TA("た", "タ", "ta"),
+    CHI("ち", "チ", "chi"),
+    TSU("つ", "ツ", "tsu"),
+    TE("て", "テ", "te"),
+    TO("と", "ト", "to"),
 
-    NA('な', 'ナ', "na"),
-    NI('に', 'ニ', "ni"),
-    NU('ぬ', 'ヌ', "nu"),
-    NE('ね', 'ネ', "ne"),
-    NO('の', 'ノ', "no"),
+    NA("な", "ナ", "na"),
+    NI("に", "ニ", "ni"),
+    NU("ぬ", "ヌ", "nu"),
+    NE("ね", "ネ", "ne"),
+    NO("の", "ノ", "no"),
 
-    HA('は', 'ハ', "ha"),
-    HI('ひ', 'ヒ', "hi"),
-    FU('ふ', 'フ', "fu"),
-    HE('へ', 'ヘ', "he"),
-    HO('ほ', 'ホ', "ho"),
+    HA("は", "ハ", "ha"),
+    HI("ひ", "ヒ", "hi"),
+    FU("ふ", "フ", "fu"),
+    HE("へ", "ヘ", "he"),
+    HO("ほ", "ホ", "ho"),
 
-    BA('ば', 'バ', "ba"),
-    BI('び', 'ビ', "bi"),
-    BU('ぶ', 'ブ', "bu"),
-    BE('べ', 'ベ', "be"),
-    BO('ぼ', 'ボ', "bo"),
+    BA("ば", "バ", "ba"),
+    BI("び", "ビ", "bi"),
+    BU("ぶ", "ブ", "bu"),
+    BE("べ", "ベ", "be"),
+    BO("ぼ", "ボ", "bo"),
 
-    PA('ぱ', 'パ', "pa"),
-    PI('ぴ', 'ピ', "pi"),
-    PU('ぷ', 'プ', "pu"),
-    PE('ぺ', 'ペ', "pe"),
-    PO('ぽ', 'ポ', "po"),
+    PA("ぱ", "パ", "pa"),
+    PI("ぴ", "ピ", "pi"),
+    PU("ぷ", "プ", "pu"),
+    PE("ぺ", "ペ", "pe"),
+    PO("ぽ", "ポ", "po"),
 
-    MA('ま', 'マ', "ma"),
-    MI('み', 'ミ', "mi"),
-    MU('む', 'ム', "mu"),
-    ME('め', 'メ', "me"),
-    MO('も', 'モ', "mo"),
+    MA("ま", "マ", "ma"),
+    MI("み", "ミ", "mi"),
+    MU("む", "ム", "mu"),
+    ME("め", "メ", "me"),
+    MO("も", "モ", "mo"),
 
-    YA('や', 'ヤ', "ya"),
-    YU('ゆ', 'ユ', "yu"),
-    YO('よ', 'ヨ', "yo"),
+    YA("や", "ヤ", "ya"),
+    YU("ゆ", "ユ", "yu"),
+    YO("よ", "ヨ", "yo"),
 
-    RA('ら', 'ラ', "ra"),
-    RI('り', 'リ', "ri"),
-    RU('る', 'ル', "ru"),
-    RE('れ', 'レ', "re"),
-    RO('ろ', 'ロ', "ro"),
+    RA("ら", "ラ", "ra"),
+    RI("り", "リ", "ri"),
+    RU("る", "ル", "ru"),
+    RE("れ", "レ", "re"),
+    RO("ろ", "ロ", "ro"),
 
-    WA('わ', 'ワ', "wa"),
-    WO('を', 'ヲ', "wo"),
+    WA("わ", "ワ", "wa"),
+    WO("を", "ヲ", "wo"),
+    N("ん", "ン", "n"),
 
-    N('ん', 'ン', "n"),
+    KYA("きゃ", "キャ", "kya"),
+    KYU("きゅ", "キュ", "kyu"),
+    KYO("きょ", "キョ", "kyo"),
 
-    KYA('きゃ', 'キャ', "kya"),
-    KYU('きゅ', 'キュ', "kyu"),
-    KYO('きょ', 'キョ', "kyo"),
+    SHA("しゃ", "シャ", "sha"),
+    SHU("しゅ", "シュ", "shu"),
+    SHO("しょ", "ショ", "sho"),
 
-    SHA('しゃ', 'シャ', "sha"),
-    SHU('しゅ', 'シュ', "shu"),
-    SHO('しょ', 'ショ', "sho"),
+    CHA("ちゃ", "チャ", "cha"),
+    CHU("ちゅ", "チュ", "chu"),
+    CHO("ちょ", "チョ", "cho"),
 
-    CHA('ちゃ', 'チャ', "cha"),
-    CHU('ちゅ', 'チュ', "chu"),
-    CHO('ちょ', 'チョ', "cho"),
+    NYA("にゃ", "ニャ", "nya"),
+    NYU("にゅ", "ニュ", "nyu"),
+    NYO("にょ", "ニョ", "nyo"),
 
-    NYA('にゃ', 'ニャ', "nya"),
-    NYU('にゅ', 'ニュ', "nyu"),
-    NYO('にょ', 'ニョ', "nyo"),
+    HYA("ひゃ", "ヒャ", "hya"),
+    HYU("ひゅ", "ヒュ", "hyu"),
+    HYO("ひょ", "ヒョ", "hyo"),
 
-    HYA('ひゃ', 'ヒャ', "hya"),
-    HYU('ひゅ', 'ヒュ', "hyu"),
-    HYO('ひょ', 'ヒョ', "hyo"),
+    MYA("みゃ", "ミャ", "mya"),
+    MYU("みゅ", "ミュ", "myu"),
+    MYO("みょ", "ミョ", "myo"),
 
-    MYA('みゃ', 'ミャ', "mya"),
-    MYU('みゅ', 'ミュ', "myu"),
-    MYO('みょ', 'ミョ', "myo"),
+    RYA("りゃ", "リャ", "rya"),
+    RYU("りゅ", "リュ", "ryu"),
+    RYO("りょ", "リョ", "ryo"),;
 
-    RYA('りゃ', 'リャ', "rya"),
-    RYU('りゅ', 'リュ', "ryu"),
-    RYO('りょ', 'リョ', "ryo"),;
-
-    public char hiragana;
-    public char katakana;
+    public String hiragana;
+    public String katakana;
     public String roomaji;
 
-    JChar(char hiragana, char katakana, String roomaji) {
+    JChar(String hiragana, String katakana, String roomaji) {
         this.hiragana = hiragana;
         this.katakana = katakana;
         this.roomaji = roomaji;
     }
 
-    public char getHiragana() {
+    public String getHiragana() {
         return this.hiragana;
     }
 
-    public char getKatakana() {
+    public String getKatakana() {
         return this.katakana;
     }
 
--- app/src/main/java/com/github/insane96/katahiraflashcards/JGroup.java
+++ app/src/main/java/com/github/insane96/katahiraflashcards/JGroup.java
@@ -1,16 +1,49 @@
 package com.github.insane96.katahiraflashcards;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class JGroup {
-    public String name;
-    public List<JChar> charList;
+public enum JGroup {
+    VOWELS("vowels", List.of(JChar.A, JChar.I, JChar.U, JChar.E, JChar.O)),
+    K("k", List.of(JChar.KA, JChar.KI, JChar.KU, JChar.KE, JChar.KO)),
+    G("g", List.of(JChar.GA, JChar.GI, JChar.GU, JChar.GE, JChar.GO)),
+    S("s", List.of(JChar.SA, JChar.SHI, JChar.SU, JChar.SE, JChar.SO)),
+    Z("z", List.of(JChar.ZA, JChar.JI, JChar.ZU, JChar.ZE, JChar.ZO)),
+    T("t", List.of(JChar.TA, JChar.CHI, JChar.TSU, JChar.TE, JChar.TO)),
+    N("n", List.of(JChar.NA, JChar.NI, JChar.NU, JChar.NE, JChar.NO)),
+    H("h", List.of(JChar.HA, JChar.HI, JChar.FU, JChar.HE, JChar.HO)),
+    B("b", List.of(JChar.BA, JChar.BI, JChar.BU, JChar.BE, JChar.BO)),
+    P("p", List.of(JChar.PA, JChar.PI, JChar.PU, JChar.PE, JChar.PO)),
+    M("m", List.of(JChar.MA, JChar.MI, JChar.MU, JChar.ME, JChar.MO)),
+    Y("y", List.of(JChar.YA, JChar.YU, JChar.YO)),
+    R("r", List.of(JChar.RA, JChar.RI, JChar.RU, JChar.RE, JChar.RO)),
+    WN("wn", List.of(JChar.WA, JChar.WO, JChar.N)),
+    KY("ky", List.of(JChar.KYA, JChar.KYU, JChar.KYO)),
+    SH("sh", List.of(JChar.SHA, JChar.SHU, JChar.SHO)),
+    CH("ch", List.of(JChar.CHA, JChar.CHU, JChar.CHO)),
+    NY("ny", List.of(JChar.NYA, JChar.NYU, JChar.NYO)),
+    HY("hy", List.of(JChar.HYA, JChar.HYU, JChar.HYO)),
+    MY("my", List.of(JChar.MYA, JChar.MYU, JChar.MYO)),
+    RY("ry", List.of(JChar.RYA, JChar.RYU, JChar.RYO));
 
-    public JGroup(String name) {
-        this.name = name;
+    private static final Map<String, JGroup> NAME_MAP = new HashMap<>();
+
+    public final String name;
+    public final List<JChar> jChars;
+
+    static {
+        for (JGroup group : values()) {
+            NAME_MAP.put(group.name, group);
+        }
     }
 
-    public void addChar(JChar jChar) {
-        this.charList.add(jChar);
+    JGroup(String name, List<JChar> jChars) {
+        this.name = name;
+        this.jChars = jChars;
+    }
+
+    public static JGroup fromName(String name) {
+        return NAME_MAP.get(name);
     }
 }
