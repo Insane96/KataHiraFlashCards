5d227e42619ef5af0a638b0ce3e456af527425db
Insane96 <delvillano.alberto@gmail.com>
2025-02-10 09:32:22 +0100

    1.1.0 - version code 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.github.insane96.katahiraflashcards"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
